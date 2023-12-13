75165d7638e894122b4c078ee0c1e72d48b28450
Ceek350 <hafidzrivan4@gmail.com>
2023-12-13 11:18:22 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, url_for
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template("home.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template("register.html")
+
+@app.route('/about_us')
+def about():
+    return render_template("AboutUs.html")
+
+if __name__== '__main__':
+    app.run(debug=True)
