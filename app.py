9d440ea433a2cad612a0f6ce3912fd600dae2c90
Ceek350 <hafidzrivan4@gmail.com>
2023-12-20 18:18:00 +0700

    db

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, redirect, request, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import MySQLdb.cursors, re, hashlib
 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
+
+app.secret_key = 'komikustore'
+
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = 'sql.freedb.tech'
+app.config['MYSQL_USER'] = 'freedb_zerostore'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_PASSWORD'] = '9XcMJypct#5tX&J'
+app.config['MYSQL_DB'] = 'freedb_zerostore'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def anu():
@@ -14,11 +27,79 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    # Output message if something goes wrong...
+    msg = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        email = request.form['email']
+        password = request.form['password']
+        # Retrieve the hashed password
+        hash = password + app.secret_key
+        hash = hashlib.sha1(hash.encode())
+        password = hash.hexdigest()
+
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user_data WHERE email = %s AND password = %s', (email, password,))
+        # Fetch one record and return the result
+        account = cursor.fetchone()
+
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['email'] = account['email']
+            # Redirect to home page
+            return 'Logged in successfully!'
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect email/password!'
+    return render_template('login.html', msg='')
+
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('email', None)
+    # Redirect to login page
+    return render_template('index.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user_data WHERE email = %s', (email,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Hash the password
+            hash = password + app.secret_key
+            hash = hashlib.sha1(hash.encode())
+            password = hash.hexdigest()
+            # Account doesn't exist, and the form data is valid, so insert the new account into the accounts table
+            cursor.execute('INSERT INTO user_data (id, email, password) VALUES (NULL, %s, %s)', (email, password,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered! now u can login'
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    # Check if account exists using MySQL
+    return render_template('register.html', msg=msg)
 
 @app.route('/about_us')
 def about():
